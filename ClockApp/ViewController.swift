f382965e6032c11af2201ff3188cf92881f78529
yaowenwang <yaowen.developing@gmail.com>
2023-02-15 17:48:29 -0800

    Initial Commit

---
+++ ClockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockApp
+//
+//  Created by Yaowen Wang on 2/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
